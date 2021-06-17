a9e1763c5928a576bb7d061e2b729008d7585136
Akhil mon <akhilmon@hotmail.com>
2021-06-17 13:35:22 +0530

    Added NAme Field ON Departement Class

--- ModelDemo/Department.cs
+++ ModelDemo/Department.cs
@@ -9,6 +9,7 @@ namespace ModelDemo
   public  class Department
     {
         public int ID { get; set; }
+        public string Name { get; set; }
         public List<Student> Students { get; set; }
     }
 }
