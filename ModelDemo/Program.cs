4fa38e2bb5c0e41c829cffb8193bc6b6033f1643
Akhil mon <akhilmon@hotmail.com>
2021-06-17 13:31:39 +0530

    Add project files.

---
+++ ModelDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Environment.CurrentDirectory);
+          //Department d=  JsonConvert.DeserializeObject<Department>(File.ReadAllText(@"C:\Users\Akhil\Desktop\studentdata.txt"));
+            List<Department> students = JsonConvert.DeserializeObject<List<Department>>(File.ReadAllText(Environment.CurrentDirectory+"\\TestData.txt"));
+            Console.ReadKey();
+        }
+    }
+}
